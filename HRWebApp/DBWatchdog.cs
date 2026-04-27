0559e381f6007855b9cb5c22c95764b6b1a3743a
tranhoainam298 <namnam.trannam@gmail.com>
2026-04-27 20:03:23 +0700

    HR app

---
+++ HRWebApp/DBWatchdog.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading;
+using System.Web;
+
+public static class DBWatchdog
+{
+    public static volatile bool DatabaseAlive = false;
+
+    static DBWatchdog()
+    {
+        Thread thread = new Thread(CheckDatabase);
+        thread.IsBackground = true;
+        thread.Start();
+    }
+
+    private static void CheckDatabase()
+    {
+        string connStr = ConfigurationManager.ConnectionStrings["HRDB"].ConnectionString;
+
+        while (true)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connStr))
+                {
+                    conn.Open();
+                    DatabaseAlive = true;
+                }
+            }
+            catch
+            {
+                DatabaseAlive = false;
+            }
+
+            Thread.Sleep(1000);
+        }
+    }
+}
---
+++ HRWebApp/Models/PagedResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HRWebApp
+{
+    public class PagedResult<T>
+    {
+        public List<T> Items { get; set; }
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+        public int TotalRecords { get; set; } // thêm dòng này
+    }
+}
